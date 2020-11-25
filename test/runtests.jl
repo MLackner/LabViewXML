164e549495044acf3009f12d2fac954c054ee850
MLackner <lacknersmichael@gmail.com>
2020-11-25 18:57:14 +0100

    test cleanup

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,9 @@
 using Test
-using BenchmarkTools
 import LabViewXML
 
-println("testfile parsing benchmarking ...")
-#   113.140 μs (264 allocations: 14.94 KiB)
-d = @btime LabViewXML.readxml(joinpath(@__DIR__, "testdata/testdata.xml"))
-@test d["MyI8"] == Int8(4)
-@test d[""] == Int32(-1)
-@test d["MyCluster"]["MyU8Array"] == UInt8[1,2,3]
+@testset "LabViewXML Tests" begin
+    d = LabViewXML.readxml(joinpath(@__DIR__, "testdata/testdata.xml"))
+    @test d["MyI8"] == Int8(4)
+    @test d[""] == Int32(-1)
+    @test d["MyCluster"]["MyU8Array"] == UInt8[1,2,3]
+end
